3b0d2c5fd1933cc02831ab8397fae4224cf9c5e9
Thomas Sauer <t-sauer@outlook.de>
2018-06-04 22:02:11 +0200

    Fixed debugging launch options

--- src/extension.ts
+++ src/extension.ts
@@ -14,7 +14,7 @@ export async function activate(context: ExtensionContext) {
   // The server is implemented in node
   let serverModule = context.asAbsolutePath(path.join('node_modules', '@emberwatch', 'ember-language-server', 'lib', 'start-server.js'));
   // The debug options for the server
-  let debugOptions = { execArgv: ["--nolazy", "--debug=6004"] };
+  let debugOptions = { execArgv: ["--nolazy", "--inspect=6004"] };
 
   // If the extension is launched in debug mode then the debug server options are used
   // Otherwise the run options are used
