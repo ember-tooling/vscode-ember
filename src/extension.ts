2dbe0cbda698b7a06c8325e4d558402d80097960
Aleksandr Kanunnikov <lifeart92@gmail.com>
2019-02-14 12:22:10 +0300

    renaming

--- src/extension.ts
+++ src/extension.ts
@@ -12,7 +12,7 @@ import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, T
 export async function activate(context: ExtensionContext) {
 
   // The server is implemented in node
-  let serverModule = context.asAbsolutePath(path.join('node_modules', '@emberwatch', 'ember-language-server', 'lib', 'start-server.js'));
+  let serverModule = context.asAbsolutePath(path.join('node_modules', 'ember-language-server', 'lib', 'start-server.js'));
   // The debug options for the server
   let debugOptions = { execArgv: ["--nolazy", "--inspect=6004"] };
 
@@ -31,12 +31,12 @@ export async function activate(context: ExtensionContext) {
   let clientOptions: LanguageClientOptions = {
     // Register the server for plain text documents
     documentSelector: ['handlebars', 'javascript'],
-    outputChannelName: 'Ember Language Server',
+    outputChannelName: 'Unstable Ember Language Server',
     revealOutputChannelOn: RevealOutputChannelOn.Never
   };
 
   // Create the language client and start the client.
-  let disposable = new LanguageClient('emberLanguageServer', 'Ember Language Server', serverOptions, clientOptions).start();
+  let disposable = new LanguageClient('emberLanguageServer', 'Unstable Ember Language Server', serverOptions, clientOptions).start();
 
   // Push the disposable to the context's subscriptions so that the
   // client can be deactivated on extension deactivation
