4e3153777fadc0fb5bec7477f30c8984695b594d
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-05-30 16:08:59 +0300

    1.5.1

--- src/lenses.ts
+++ src/lenses.ts
@@ -9,83 +9,106 @@ import {
   Position
 } from "vscode";
 
-import { COMMANDS as ELS_COMMNDS } from './constants';
+export type MatchResultType =
+  | 'helper'
+  | 'service'
+  | 'route'
+  | 'controller'
+  | 'modifier'
+  | 'template'
+  | 'component'
+  | 'model'
+  | 'transform'
+  | 'adapter'
+  | 'serializer';
+type MatchResultScope = 'addon' | 'application';
+type MatchResultKind = 'test' | 'script' | 'template' | 'style';
+interface MatchResult {
+  type: MatchResultType;
+  name: string;
+  scope: MatchResultScope;
+  kind: MatchResultKind;
+}
+
+interface Result {
+  meta: MatchResult;
+  path: string;
+}
+
+import { COMMANDS as ELS_COMMANDS } from './constants';
 
 function normalizePath(str: string): string {
-    return str.split("\\").join("/");
+  return str.split("\\").join("/");
 }
 
-function lenseNameFromPath(document, f) {
-    let normPath = normalizePath(f);
-    let fsPath = normalizePath(document.uri.fsPath);
-    const isActive = fsPath === normPath;
-    const isTest = normPath.includes("/tests/");
-    const isAddon = normPath.includes("/addon/");
-    const isAddonExport = (normPath.includes('/lib/') || normPath.includes('/node_modules/')) && normPath.includes('/app/');
-    const isComponent = normPath.includes("/components/");
-    let name = normPath.endsWith(".hbs") ? "template" : "script";
-    if (
-      normPath.endsWith(".css") ||
-      normPath.endsWith(".less") ||
-      normPath.endsWith(".scss")
-    ) {
-      name = "style";
+function getActiveMeta(document, results: Result[]): MatchResult | null {
+  let fsPath = normalizePath(document.uri.fsPath);
+
+  for (let i = 0; i < results.length; i++) {
+    let normPath = normalizePath(results[i].path);
+    if (normPath === fsPath) {
+      return results[i].meta;
     }
-    if (!isComponent) {
-      if (normPath.includes("/routes/")) {
-        name = "route";
-      } else if (normPath.includes("/controllers/")) {
-        name = "controller";
-      }
-    }
-    if (isAddon) {
-      name = `addon:${name}`;
-    }
-    if (isAddonExport) {
-      name = `addon-app:${name}`;
-    }
-    if (isTest) {
-      if (["script", "template"].includes(name)) {
-        name = "test";
-      } else {
-        name = `${name}:test`;
-      }
-    }
-    if (isActive) {
-      name = "_" + name + "_";
-    } else {
-      name = " " + name + " ";
-    }
-    return name;
+  }
+
+  return null;
+}
+function lenseNameFromPath(document, f: Result, meta: MatchResult | null) {
+  let normPath = normalizePath(f.path);
+  let fsPath = normalizePath(document.uri.fsPath);
+  const isActive = fsPath === normPath;
+  const isAddonApp = normPath.includes("/app/");
+  const isAddonExport = f.meta.scope === 'addon';
+  const isRoutePath = ['route', 'controller', 'template'].includes(f.meta.type);
+  const typeKey = isRoutePath ? `:${f.meta.type}:` : ':';
+  const scope = isAddonExport && isAddonApp ? 'addon-app' : f.meta.scope;
+  const key = `${scope}${typeKey}${f.meta.kind}`;
+  let name = key.replace('application:', '');
+  name = name.replace('template:template', 'template');
+  if (meta && !isRoutePath) {
+    name = name.replace(`${meta.type}:`, '');
+  }
+  name = name.replace(`${f.meta.type}:script`, f.meta.type);
+  if (isActive) {
+    name = "_" + name + "_";
+  } else {
+    name = " " + name + " ";
+  }
+  return name;
 }
 
-export  async function provideCodeLenses(
-    document: TextDocument,
-    token: CancellationToken
-  ) {
-    let relatedFiles: any = [];
-    try {
-      relatedFiles = await commands.executeCommand(
-        ELS_COMMNDS.GET_RELATED_FILES,
-        document.uri.fsPath
-      );
-      if (token.isCancellationRequested) {
-        return;
-      }
-      // window.showInformationMessage(JSON.stringify(relatedFiles));
-    } catch (e) {
-      // window.showErrorMessage(e.toString());
-    }
-    if (relatedFiles.length === 1) {
+export async function provideCodeLenses(
+  document: TextDocument,
+  token: CancellationToken
+) {
+  let relatedFiles: any = [];
+  try {
+    relatedFiles = await commands.executeCommand(
+      ELS_COMMANDS.GET_RELATED_FILES,
+      document.uri.fsPath,
+      { includeMeta: true }
+    );
+    if (token.isCancellationRequested) {
       return;
     }
-    return relatedFiles.map(f => {
-      const name = lenseNameFromPath(document, f);
-      return new CodeLens(new Range(new Position(0, 0), new Position(0, 0)), {
-        title: name,
-        tooltip: "Ember: " + f,
-        command: ELS_COMMNDS.OPEN_RELATED_FILE,
-        arguments: f.split("")
-      });
-    });
+    // window.showInformationMessage(JSON.stringify(relatedFiles));
+  } catch (e) {
+    // window.showErrorMessage(e.toString());
   }
+  if (relatedFiles.length === 1) {
+    return;
+  }
+  const meta = getActiveMeta(document, relatedFiles);
+  const lenseNames = relatedFiles.map((f: Result) => {
+    const name = lenseNameFromPath(document, f, meta);
+    return { name, path: f.path.split(""), tooltip: f.path };
+  })
+  return lenseNames.map(({ name, path, tooltip }) => {
+    return new CodeLens(new Range(new Position(0, 0), new Position(0, 0)), {
+      title: name,
+      tooltip: "Ember: " + tooltip,
+      command: ELS_COMMANDS.OPEN_RELATED_FILE,
+      arguments: path
+    });
+  });
+}
